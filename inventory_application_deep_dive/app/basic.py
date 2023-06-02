06d0a8114d5a6230165ee409a00548626dfc7816
pavlo <pavlo_ivanov@yahoo.com>
2023-06-02 17:11:39 +0200

    correcting error message1

--- inventory_application_deep_dive/app/basic.py
+++
@@ -1,6 +0,0 @@
-# basic
-
-
-def addition(a, b):
-    """Basic function to test that CI works with conftest"""
-    return a + b
--- inventory_application_deep_dive/tests/test_basic.py
+++
@@ -1,9 +0,0 @@
-# test_basic.py
-from inventory_application_deep_dive.app.basic import addition
-
-
-def test_basic_basic(demo_fixt) -> None:
-    """Basic test to test Continuous Integration works correctly."""
-    a, b, expected = demo_fixt
-    result = addition(a, b)
-    assert result == expected
