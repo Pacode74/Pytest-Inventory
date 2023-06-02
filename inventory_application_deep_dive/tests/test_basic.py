b08e8d3b44c976418ec1d98e46287627ee27a314
pavlo <pavlo_ivanov@yahoo.com>
2023-06-02 13:00:07 +0200

    Modified github-action.yml and bitbucket-pipelines.yml. Created basic tests in intentory_aaplication_deep_dive and inventory_application_store_multiple_items to test continues integration.

---
+++ inventory_application_deep_dive/tests/test_basic.py
@@ -0,0 +1,9 @@
+# test_basic.py
+from inventory_application_deep_dive.apps.basic import addition
+
+
+def test_basic(demo_fixt) -> None:
+    """Basic test to test Continuous Integration works correctly."""
+    a, b, expected = demo_fixt
+    result = addition(a, b)
+    assert result == expected
---
+++ inventory_application_store_multiple_items/apps/basic.py
@@ -0,0 +1,6 @@
+# basic
+
+
+def addition(a, b):
+    """Basic function to test that CI works with conftest"""
+    return a + b
